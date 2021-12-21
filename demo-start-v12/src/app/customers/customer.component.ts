94ad10fcf3cf421bd4c6d4444aec5fb1dabee366
TheRubble <therubble@hotmail.com>
2021-12-21 14:49:33 +0000

    Switched to a reactive form

--- demo-start-v12/src/app/customers/customer.component.ts
+++ demo-start-v12/src/app/customers/customer.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit } from '@angular/core';
-import { NgForm } from '@angular/forms';
+import {FormGroup, FormControl, FormBuilder} from '@angular/forms';
 
 import { Customer } from './customer';
 
@@ -9,15 +9,40 @@ import { Customer } from './customer';
   styleUrls: ['./customer.component.css']
 })
 export class CustomerComponent implements OnInit {
-  customer = new Customer();
 
-  constructor() { }
+  customerForm! : FormGroup;
+  customer = new Customer();
+  constructor(private formBuilder : FormBuilder) { }
 
   ngOnInit(): void {
+    // this.customerForm = new FormGroup({
+    //     firstName : new FormControl(),
+    //     lastName : new FormControl(),
+    //     emailAddress : new FormControl(),
+    //     sendCatalog : new FormControl(true)
+    //   }
+    // );
+
+    this.customerForm =  this.formBuilder.group({
+      firstName : null,
+      lastName : { value : "", disabled : false},
+      emailAddress : null,
+      sendCatalog : true
+    })
+
   }
 
-  save(customerForm: NgForm): void {
-    console.log(customerForm.form);
-    console.log('Saved: ' + JSON.stringify(customerForm.value));
+  save(): void {
+    console.log(this.customerForm);
+    console.log('Saved: ' + JSON.stringify(this.customerForm.value));
+  }
+
+  populateTestData() {
+    this.customerForm.setValue({
+      firstName : "Chriss",
+      lastName : "Barnard",
+      emailAddress : "therubble@hotmail.com",
+      sendCatalog : false
+    })
   }
 }
